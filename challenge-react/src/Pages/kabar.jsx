4975400fc401d4e0a23ef44f4523b1aeae9dbf4f
Fadhil Abdulkarim <fadhil@alterra.id>
2020-01-07 15:58:13 +0700

    nambahin filter kalau image kosong tapi ada broken link juga

--- challenge-react/src/Pages/kabar.jsx
+++ challenge-react/src/Pages/kabar.jsx
@@ -8,7 +8,7 @@ import ComponentListArticle from "../Components/listArticle"
 // USAGE FOR AXIOS
 const apiKey = "485a6bc08eca4546a902aa3f87808b0c";
 const baseUrl = "https://newsapi.org/v2";
-const urlCombined = baseUrl + "/top-headlines?country=id&pageSize=5&apiKey=" + apiKey;
+const urlCombined = baseUrl + "/top-headlines?country=id&apiKey=" + apiKey;
 // const newUrl = baseUrl + "/top-headlines?country=id"
 class PageKabar extends Component {
     state = {
@@ -19,22 +19,31 @@ class PageKabar extends Component {
         nowTime: new Date()
     };
 
+    filterOut = () => {
+        const self = this;
+        const scoop = this.state.mainList.filter(function(item){
+            return item.urlToImage != null
+        })
+        self.setState({ mainList: scoop })
+    }
+
     reload = (category, ...restArgument) => {
         const self = this;
         // e.preventDefult();
         let newUrl
         (restArgument.length === 0) ? newUrl = urlCombined + "&category=" + category : newUrl = urlCombined + "&search=" + restArgument[0]
-        axios.get(urlCombined)
+        axios.get(urlCombined + "&pageSize=5")
             .then(function (response) {
                 self.setState({ listNews: response.data.articles, isLoading: false });
             })
             .catch()
-        axios.get(newUrl)
+        axios.get(newUrl + "&pageSize=10")
             .then(function (response) {
                 self.setState({ mainList: response.data.articles, mainLoading: false});
             })
             .catch()
         self.setState({ nowTime: new Date() })
+        self.filterOut()
     }
 
     componentDidMount = () => {
